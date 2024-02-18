8e49830240acc9fedf73be42bda40d412cdce151
hadekha <hadekha.fitra@pactindo.com>
2024-02-18 18:59:49 +0700

    use image v1

--- build_push_image.sh
+++ build_push_image.sh
@@ -1,36 +1,24 @@
 #! /bin/bash
 
-# if error return err immediately, do not execute next line
-set -e
-
-# get latest commit sha
-
-echo "latest commit sha256:"
-git log -1 --format="%H"
-commitsha=$(git log -1 --format="%H")
-
-# build image with tag from commit
-docker build --tag "ghcr.io/hdkef/item-app:$commitsha" .
-
-# list all images
-docker images
-
 # if error do not return err immediately
 set +e
 
 # remove latest tag and ignore error log
-docker rmi ghcr.io/hdkef/item-app:latest 2>/dev/null
+docker rmi ghcr.io/hdkef/item-app:v1 2>/dev/null
 
 # if error return err immediately, do not execute next line
 set -e
 
-# create latest tag from image with tag from commit
-docker tag "ghcr.io/hdkef/item-app:$commitsha" "ghcr.io/hdkef/item-app:latest"
+# build image with tag from commit
+docker build --tag "ghcr.io/hdkef/item-app:v1" .
 
-# push latest images
+# list all images
+docker images
+
+# push v1 images
 
 # login to github container registry
 echo $PASSWORD_DOCKER_HUB | docker login ghcr.io -u hdkef11@gmail.com --password-stdin
 
 # push images
-docker push ghcr.io/hdkef/item-app:latest
+docker push ghcr.io/hdkef/item-app:v1
