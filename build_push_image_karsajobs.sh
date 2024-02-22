2513be6236b965358f390c141c4c78881d46e1a3
hadekha.fitra@pactindo.com <hadekha.fitra@pactindo.com>
2024-02-22 09:40:00 +0700

    add build & push image script

---
+++ build_push_image_karsajobs.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+#if error do not execute next line
+set -e
+
+# build image
+docker build -f=Dockerfile --tag=ghcr.io/hdkef/karsajobs:latest  .
+
+# login to container registry
+echo $PASSWORD_DOCKER_HUB | docker login ghcr.io -u hdkef11@gmail.com --password-stdin
+
+# push image to container registry
+docker push ghcr.io/hdkef/karsajobs:latest
